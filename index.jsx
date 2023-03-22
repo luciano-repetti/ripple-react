1d0513bedb7c9faac3057998075dc22be2b40a6b
Dipu <dipuchaurasiya91@gmail.com>
2023-03-22 18:47:48 +0545

    bug fixed

--- index.jsx
+++ index.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import "./style.css";
 
-function RippleButton({
-  children,
-  color = "rgb(83, 82, 82)",
-  speed = 1000,
-  radius = 2,
-}) {
+function RippleButton({ children, color = "red", speed = 1000, radius = 2 }) {
   function createRipple(event) {
     // Create a new ripple element
     const button = event.currentTarget;
     const ripple = document.createElement("div");
     button.appendChild(ripple);
-
     let d = Math.max(button.clientWidth, button.clientHeight);
     ripple.style.width = ripple.style.height = d + "px";
-    ripple.style.left = event.clientX - button.offsetLeft - d / 2 + "px";
-    ripple.style.top = event.clientY - button.offsetTop - d / 2 + "px";
+    const buttonRect = button.getBoundingClientRect();
+    ripple.style.left = event.clientX - buttonRect.left - d / 2 + "px";
+    ripple.style.top = event.clientY - buttonRect.top - d / 2 + "px";
     ripple.style.backgroundColor = color;
     ripple.style.animationDuration = `${speed}ms`;
     ripple.classList.add("ripple");
@@ -26,13 +21,13 @@ function RippleButton({
   }
 
   return (
-    <button
+    <div
       className="ripple-button"
       style={{ borderRadius: `${radius}px` }}
       onClick={createRipple}
     >
       {children}
-    </button>
+    </div>
   );
 }
 
